50bdbf62f635d386380381ff45b4d86aad2ba565
Deepak_Prasad <deepak.prasad920@gmail.com>
2025-02-06 21:16:01 +0530

    folder structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './Home';
+import About from './About';
+import Products from './Products';
+import Contact from './Contact';
+import SingleProduct from './SingleProduct';
+import Cart from './Cart';
+import ErrorPage from './ErrorPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Deepak Prasad</h1>
+      <BrowserRouter>
+        <Routes>
+            <Route path='/' element = {<Home />}></Route>
+            <Route path='/about' element={<About />}></Route>
+            <Route path='/products' element={<Products />}></Route>
+            <Route path='/contact' element={<Contact />}></Route>
+            <Route path='/singleproduct/:id' element={<SingleProduct />}></Route>
+            <Route path='/cart' element={<Cart />}></Route>
+            <Route path='*' element={<ErrorPage />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/ErrorPage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ErrorPage = () => {
+    return(
+        <p>Page Not Found.</p>
+    )
+}
+
+export default ErrorPage;
