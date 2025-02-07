b376347e93f4348def14d129b9a942254f29bbd7
Deepak_Prasad <deepak.prasad920@gmail.com>
2025-02-07 22:26:18 +0530

    api call by context api for feature product

--- src/context/ProductContext.js
+++ src/context/ProductContext.js
@@ -1,13 +1,39 @@
-import { createContext, useContext } from "react";        // 1st- Create a Context(like store)
+import { createContext, useContext, useEffect, useReducer } from "react";        // 1st- Create a Context(like store)
+import axios from "axios";
+import reducer from "../reducer/productReducer";
 
 const AppContext = createContext();           // Took createContext into a variable    
 
+const API = "https://api.pujakaitem.com/api/products";
+const initialState = {
+    isLoading: false,
+    isError: false,
+    products: [],
+    featureProducts: [],
 
-// 2nd- Created a Provider to access all app component or child component. 
-// here children is AppComponent which is Wraaped by Provider in index.js file.
-const AppProvider = ({children}) => {  
+};
+
+// 2nd- Created a Provider to access all app component or child component. here children is AppComponent which is Wraaped by Provider in index.js file.
+const AppProvider = ({children}) => { 
+    const [state, dispatch] = useReducer(reducer, initialState);
+
+    const getProducts = async (url) => {
+      dispatch({type: 'SET_LOADING'});
+      try {
+         const res = await axios.get(url);
+         const products = await res.data;
+         dispatch({type: 'SET_API_DATA', payload: products});
+      } catch (error) {
+         dispatch({type: 'API_ERROR'});
+      }
+    }
+
+    useEffect(()=>{
+        getProducts(API);
+    }, [])
+
     return (
-       <AppContext.Provider value={{ myName: "Deepak Prasad" }}>     
+       <AppContext.Provider value={{ ...state }}>     
         {children}
        </AppContext.Provider>
     );
