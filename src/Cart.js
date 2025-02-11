184f395cf87f4abd9569937e5c91f80722304610
Deepak_Prasad <deepak.prasad920@gmail.com>
2025-02-11 14:09:17 +0530

    FE DONE

--- src/Cart.js
+++ src/Cart.js
@@ -4,12 +4,14 @@ import CartItem from "./components/CartItem";
 import { NavLink } from "react-router-dom";
 import { Button } from "./styles/Button";
 import FormatPrice from "./Helpers/FormatPrice";
+import { useAuth0 } from "@auth0/auth0-react";
 
 const Cart = () => {
   const { cart, clearCart, total_price, shipping_fee } = useCartContext();
-  // console.log("🚀 ~ file: Cart.js ~ line 6 ~ Cart ~ cart", cart);
 
-  if (cart?.length === 0) {
+  const { isAuthenticated, user } = useAuth0();
+
+  if (cart.length === 0) {
     return (
       <EmptyDiv>
         <h3>No Cart in Item </h3>
@@ -20,6 +22,13 @@ const Cart = () => {
   return (
     <Wrapper>
       <div className="container">
+        {isAuthenticated && (
+          <div className="cart-user--profile">
+            <img src={user.profile} alt={user.name} />
+            <h2 className="cart-user--name">{user.name}</h2>
+          </div>
+        )}
+
         <div className="cart_heading grid grid-five-column">
           <p>Item</p>
           <p className="cart-hide">Price</p>
@@ -29,7 +38,7 @@ const Cart = () => {
         </div>
         <hr />
         <div className="cart-item">
-          {cart?.map((curElem) => {
+          {cart.map((curElem) => {
             return <CartItem key={curElem.id} {...curElem} />;
           })}
         </div>
--- src/Contact.js
+++ src/Contact.js
@@ -1,6 +1,9 @@
+import { useAuth0 } from "@auth0/auth0-react";
 import styled from "styled-components";
 
 const Contact = () => {
+  const { isAuthenticated, user } = useAuth0();
+
   const Wrapper = styled.section`
     padding: 9rem 0 5rem 0;
     text-align: center;
@@ -38,7 +41,7 @@ const Contact = () => {
       <h2 className="common-heading">Contact page</h2>
 
       <iframe
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3679.5055209505113!2d75.9340344902869!3d22.746613455666406!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3962e3cd9d3de1ad%3A0xebaf3ee69f583ff5!2sPhoenix%20Citadel%20Mall!5e0!3m2!1sen!2sin!4v1738923548244!5m2!1sen!2sin"
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3782.265588856342!2d73.91455641541671!3d18.562061287384868!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bc2c147b8b3a3bf%3A0x6f7fdcc8e4d6c77e!2sPhoenix%20Marketcity%20-%20Viman%20Nagar!5e0!3m2!1sen!2sin!4v1664345115285!5m2!1sen!2sin"
         width="100%"
         height="400"
         style={{ border: 0 }}
@@ -56,6 +59,7 @@ const Contact = () => {
               type="text"
               placeholder="username"
               name="username"
+              value={isAuthenticated ? user.name : ""}
               required
               autoComplete="off"
             />
@@ -65,6 +69,7 @@ const Contact = () => {
               name="Email"
               placeholder="Email"
               autoComplete="off"
+              value={isAuthenticated ? user.email : ""}
               required
             />
 
